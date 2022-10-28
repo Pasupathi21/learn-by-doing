4d2c3cf8e01176df63050f62ba82320b358bffb4
Pasupathi M <pasupathi@aaludra.com>
2022-10-28 16:42:44 +0530

    Reduce method

--- src/practices/array-methods.js
+++ src/practices/array-methods.js
@@ -1,8 +1,14 @@
 // Reduce method
 const data = require('../practices/data/dummy-data.json')
 
-// console.log("Data", data)
-
-const finalResult = data.products.reduce((acc, cur) => {
-    console.log(`Accumulator ${acc}, Current ${cur}`);
-}, {});
+// Using Reduce method Array
+let total = 0
+const finalResult = data.products.reduce((acc, cur, index, arr) => {
+    total += (cur.stock * cur.price)
+    if(data.products.length -1  == index){
+        data.products[0].stkMul = total;
+        return arr
+    }
+    
+});
+console.log("Ans: ", finalResult);
